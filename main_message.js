0cacb001e58f40a3f749e422285e0f28d5d691f6
Paula Sosa <paula.sosa.mt@usa.edu.co>
2022-09-23 19:10:29 -0500

    Agrego JavaScript para box message

--- main_message.js
+++ main_message.js
@@ -37,3 +37,139 @@ https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/or
 
 */
 
+function getInfoMessage() {
+    $.ajax({
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*"
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat"
+        , type: 'GET'
+        , dataType : 'json',
+
+        success: function (respuesta) {
+            console.log(respuesta);
+        },
+    error : function(xhr, status) {
+        alert('Sucedió un problema');
+        console.log(status);
+    },
+    complete : function(xhr, status) {
+        alert('Petición realizada');
+        console.log("Todo Ok")
+    }
+});
+}
+
+
+function addInfoMessage() {
+    var misdatosentexto = '{"id":' + $("#idmensaje").val() + ',' + '"messagetext":"' + $("#messagetext").val() + '"}';
+    var misdatoenformatojson = JSON.parse(misdatosentexto);
+
+    $.ajax({
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*"
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat"
+        , type: 'POST'
+        , data:misdatoenformatojson
+        , dataType : 'JSON',
+
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert('Agregado');
+        },
+    error : function(xhr, status) {
+        alert('Sucedió un problema');
+        console.log(status);
+    },
+    complete : function(xhr, status) {
+        alert('Petición realizada');
+        console.log("Todo Ok")
+    }
+});    
+    
+}
+
+function modInfoMessage() {
+    var misdatosentexto = '{"id":' + $("#idmensaje").val() + ',' + '"messagetext":"' + $("#messagetext").val() + '"}';
+    var misdatoenformatojson = JSON.parse(misdatosentexto);
+
+    $.ajax({
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*"
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat"
+        , type: 'PUT'
+        , data:misdatoenformatojson
+        , dataType : 'JSON',
+
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert('Agregado');
+        },
+    error : function(xhr, status) {
+        alert('Sucedió un problema');
+        console.log(status);
+    },
+    complete : function(xhr, status) {
+        alert('Petición realizada');
+        console.log("Todo Ok")
+    }
+});    
+    
+}
+
+function getInfoUnMessage() {
+    var misdatosentexto = $("#idmensaje").val();
+
+    $.ajax({
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*"
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat" + "/" + misdatosentexto
+        , type: 'GET'
+        , dataType : 'JSON',
+
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert('Agregado');
+        },
+    error : function(xhr, status) {
+        alert('Sucedió un problema');
+        console.log(status);
+    },
+    complete : function(xhr, status) {
+        alert('Petición realizada');
+        console.log("Todo Ok")
+    }
+});    
+    
+}
+
+
+function deleteInfoUnMessage() {
+    var misdatosentexto = $("#idmensaje").val();
+
+    $.ajax({
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*"
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat" + "/" + misdatosentexto
+        , type: 'DELETE'
+        , dataType : 'JSON',
+
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert('Agregado');
+        },
+    error : function(xhr, status) {
+        alert('Sucedió un problema');
+        console.log(status);
+    },
+    complete : function(xhr, status) {
+        alert('Petición realizada');
+        console.log("Todo Ok")
+    }
+});    
+    
+}
