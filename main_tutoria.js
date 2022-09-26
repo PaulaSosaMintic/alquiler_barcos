d31bd439da239ebf4331adfd41b485b0b5290204
Paula Sosa <paula.sosa.mt@usa.edu.co>
2022-09-26 18:36:52 -0500

    apuntes de la tutoria

--- main_tutoria.js
+++ main_tutoria.js
@@ -1,3 +1,16 @@
 /*
+Este documento se tomará en cuenta la tutoria de la clase del reto 2 para poder realizar el front end con javascript en base a los html que se ha creado
+
+Consejo 1:
+Las funciones para las tablas deben ser diferentes para evitar conflictos entre tablas.
+
+Consejo 2:
+Los id deben ser diferentes de acuerdo a la información requerida en cada tabla y así evitar conflictos.
+
+Consejo 3:
+Tener pendiente el URL de cada tabla para incluir el correcto en cada función.
+
+
 
 */
+
