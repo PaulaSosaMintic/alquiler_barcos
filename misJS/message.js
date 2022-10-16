68f76a772854fcab430892c948b420f49a35a17c
Paula Sosa <paula.sosa.mt@usa.edu.co>
2022-10-16 14:42:12 -0500

    variable para pintar la funcion de consultar uno

--- misJS/message.js
+++ misJS/message.js
@@ -107,18 +107,43 @@ function borrarMensajes(idElemento) {
     });
 }
 
-
 //TRAER UN ELEMENTO
     
 function traerInformacionUno() {
-    
+    let myData = {
+        id: $("#id").val()
+    }
+
+    let dataJson = JSON.stringify(myData);
+
     $.ajax({
-        url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat",
-        type: "GET",
-        datatipe: "JSON",
-    success: function (respuesta) {
-        console.log(respuesta);
-        pintarRespuesta(respuesta.items);
-    },
-    });
+            headers:{ 
+                    accept: 'application/json', "Access-Control-Allow-Origin":"*", "Content-Type": 'application/json'
+            }
+        , url: "https://g597b3e253f7383-vxwukxbb0ai8lta7.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/boat/boat" + "/" + myData
+        , type: 'GET'
+        , data: dataJson
+        , dataType : 'JSON',
+        success: function (respuesta) {
+            $("#resultado").empty();
+            console.log(respuesta);
+            pintarRespuestaUno(respuesta.items_uno);
+        }
+});    
+}
+
+function pintarRespuestaUno(items_uno) {
+    let myTable = "<table><th>ID</th> <th>MARCA</th> <th>MODELO</th> <th>CATEGORIA</th> <th>NOMBRE</th> ";
+    for (i = 0; i < items.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + items[i].id + "</td>";
+        myTable += "<td>" + items[i].brand + "</td>";
+        myTable += "<td>" + items[i].model + "</td>";
+        myTable += "<td>" + items[i].category_id + "</td>";
+        myTable += "<td>" + items[i].name + "</td>";
+        myTable += "<td> <button onclick='borrarElemento(" +items[i].id +")'>Borrar</button>";
+        myTable += "</tr>";
+    }
+        myTable += "</table>";
+    $("#resultado").append(myTable);
 }
